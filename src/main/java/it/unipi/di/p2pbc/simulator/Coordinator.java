c8a638bd04ae87b95ff3adf5e5624f826c78c977
Francesco Landolfi <fran.landolfi@gmail.com>
2018-06-21 11:14:58 +0200

    Update Coordinator

--- src/main/java/it/unipi/di/p2pbc/simulator/Coordinator.java
+++ src/main/java/it/unipi/di/p2pbc/simulator/Coordinator.java
@@ -2,40 +2,45 @@ package main.java.it.unipi.di.p2pbc.simulator;
 
 import main.java.it.unipi.di.p2pbc.newscast.Correspondent;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
 public class Coordinator<T> {
     private NetworkGenerator<T> generator;
+    private int currentStep;
+    private Logger<T> logger;
 
-    public Coordinator(NetworkGenerator<T> generator) {
+
+    public Coordinator(NetworkGenerator<T> generator, Logger<T> logger) {
         this.generator = generator;
+        this.currentStep = 0;
+        this.logger = logger;
     }
 
-    public void simulate(int size, int steps, Logger<Correspondent<T>> logger) {
-        List<Correspondent<T>> network = generator.instantiateNetwork(size);
-        HashMap<Correspondent<T>, Integer> lastUpdate = new HashMap<>();
-        network.forEach(n -> lastUpdate.put(n, -1));
-        logger.beginSession(size);
+    public void simulate(int steps) {
+        List<Correspondent<T>> network = new ArrayList<>(generator.getNetwork());
+        HashMap<Correspondent, Integer> lastUpdate = new HashMap<>();
+        generator.getNetwork().forEach(n -> lastUpdate.put(n, currentStep - 1));
 
-        for (int i = 0; i < steps; i++) {
-            Collections.shuffle(network);
-            logger.logNetworkState(network, i);
+        for (; currentStep < currentStep + steps; currentStep++) {
+            logger.logNetworkState(network, currentStep);
 
             for (Correspondent<T> node: network) {
-                if (lastUpdate.get(node) == i)
+                if (lastUpdate.get(node) == currentStep)
                     continue;
 
-                Correspondent<T> peer = node.getRandomPeer();
-                lastUpdate.put(node, i);
-                lastUpdate.put(peer, i);
+                Correspondent peer = node.getRandomPeer();
+                lastUpdate.put(node, currentStep);
+                lastUpdate.put(peer, currentStep);
 
                 node.update(peer);
             }
+
+            Collections.shuffle(network);
         }
 
         logger.logNetworkState(network, steps);
-        logger.endSession();
     }
 }
