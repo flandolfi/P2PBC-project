fba08bfdea4cda8ecdb5e8e51e8ce4cc6dcd1e98
Francesco Landolfi <fran.landolfi@gmail.com>
2018-06-27 15:36:31 +0200

    Add Lattice network

---
+++ src/main/java/it/unipi/di/p2pbc/newscast/simulation/LatticeNetwork.java
@@ -0,0 +1,16 @@
+package it.unipi.di.p2pbc.newscast.simulation;
+
+import it.unipi.di.p2pbc.newscast.core.Cache;
+
+public class LatticeNetwork<T> extends EmptyNetwork<T> {
+    protected LatticeNetwork(int size, AgentFactory<T> agentFactory) {
+        super(agentFactory);
+
+        for (int i = 0; i < size; i++)
+            addPeer();
+
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < Cache.getSize(); j++)
+                link(network.get(i), network.get((i + j - Cache.getSize()/2) % size));
+    }
+}
