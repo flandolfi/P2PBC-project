a541a4ed5e1b2fd32fb4163d2463f7e2dc072801
Francesco Landolfi <fran.landolfi@gmail.com>
2018-06-28 23:50:35 +0200

    Fix negative index

--- src/main/java/it/unipi/di/p2pbc/newscast/simulation/LatticeNetwork.java
+++ src/main/java/it/unipi/di/p2pbc/newscast/simulation/LatticeNetwork.java
@@ -3,7 +3,7 @@ package it.unipi.di.p2pbc.newscast.simulation;
 import it.unipi.di.p2pbc.newscast.core.Cache;
 
 public class LatticeNetwork<T> extends EmptyNetwork<T> {
-    protected LatticeNetwork(int size, AgentFactory<T> agentFactory) {
+    public LatticeNetwork(int size, AgentFactory<T> agentFactory) {
         super(agentFactory);
 
         for (int i = 0; i < size; i++)
@@ -11,6 +11,6 @@ public class LatticeNetwork<T> extends EmptyNetwork<T> {
 
         for (int i = 0; i < size; i++)
             for (int j = 0; j < Cache.getSize(); j++)
-                link(network.get(i), network.get((i + j - Cache.getSize()/2) % size));
+                link(network.get(i), network.get((((i + j - Cache.getSize()/2) % size) + size) % size));
     }
 }
